088f0575e63bdb80f46186ad54eefc52d98e9584
system sylvere BARINDA <systembarinda@gmail.com>
2026-08-02 20:12:10 +0200

    chore:updating at all

---
+++ src/components/FeaturedPosts.tsx
@@ -0,0 +1,35 @@
+import PostCard from "./PostCard";
+import { posts } from "../data/posts";
+
+export default function FeaturedPosts() {
+  return (
+    <section className="bg-slate-950 py-24">
+      <div className="mx-auto max-w-7xl px-6">
+        <div className="mb-16 text-center">
+          <span className="text-blue-500 font-semibold">Latest Articles</span>
+
+          <h2 className="mt-4 text-5xl font-bold text-white">Featured Posts</h2>
+
+          <p className="mx-auto mt-6 max-w-2xl text-lg text-slate-400">
+            Sharing tutorials, software engineering insights, backend
+            development experiences, and practical guides to help developers
+            grow.
+          </p>
+        </div>
+
+        <div
+          className="
+          grid
+          gap-8
+          md:grid-cols-2
+          xl:grid-cols-3
+        "
+        >
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
