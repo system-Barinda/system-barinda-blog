2b1ef17ed96eee53f582f012a493283594193ca5
system sylvere BARINDA <systembarinda@gmail.com>
2026-08-02 20:11:29 +0200

    chore:updating at all

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,86 @@
+import { ArrowRight } from "lucide-react";
+
+type Props = {
+  post: {
+    title: string;
+    category: string;
+    image: string;
+    description: string;
+    date: string;
+    readTime: string;
+  };
+};
+
+export default function PostCard({ post }: Props) {
+  return (
+    <article
+      className="
+      group
+      overflow-hidden
+      rounded-2xl
+      border
+      border-slate-800
+      bg-slate-900
+      transition-all
+      duration-300
+      hover:-translate-y-2
+      hover:border-blue-500
+      hover:shadow-2xl
+    "
+    >
+      <div className="overflow-hidden">
+        <img
+          src={post.image}
+          alt={post.title}
+          className="
+            h-56
+            w-full
+            object-cover
+            transition-transform
+            duration-500
+            group-hover:scale-110
+          "
+        />
+      </div>
+
+      <div className="p-6">
+        <span
+          className="
+          rounded-full
+          bg-blue-600/10
+          px-3
+          py-1
+          text-sm
+          text-blue-400
+        "
+        >
+          {post.category}
+        </span>
+
+        <h3 className="mt-4 text-2xl font-bold text-white">{post.title}</h3>
+
+        <p className="mt-4 leading-7 text-slate-400">{post.description}</p>
+
+        <div className="mt-6 flex justify-between text-sm text-slate-500">
+          <span>{post.readTime}</span>
+          <span>{post.date}</span>
+        </div>
+
+        <button
+          className="
+          mt-6
+          flex
+          items-center
+          gap-2
+          text-blue-400
+          transition
+          hover:gap-3
+        "
+        >
+          Read More
+          <ArrowRight size={18} />
+        </button>
+      </div>
+    </article>
+  );
+}
