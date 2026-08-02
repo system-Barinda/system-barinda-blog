e47489725fc8f63d1fbf1ec1b8d1fb6f6541c473
system sylvere BARINDA <systembarinda@gmail.com>
2026-08-02 20:12:49 +0200

    chore:updating at all

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import Hero from "../components/Hero";
+import FeaturedPosts from "../components/FeaturedPosts";
 
 export default function Home() {
   return (
     <>
       <Hero />
+
+      <FeaturedPosts />
     </>
   );
 }
