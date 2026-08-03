6843db3d62624225dc143d49a47ccee377b648d1
system sylvere BARINDA <systembarinda@gmail.com>
2026-08-03 21:31:50 +0200

    chore:updating categories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -6,11 +6,13 @@ export default function Categories() {
     <section className="bg-white py-24">
       <div className="mx-auto max-w-7xl px-6">
         <div className="mb-16 text-center">
-          <span className="font-semibold text-blue-500">Explore Topics</span>
+          <span className="text-6xl font-semibold text-green-500">
+            Explore Topics
+          </span>
 
-          <h2 className="mt-4 text-5xl font-bold text-gray-900">Categories</h2>
+          <h2 className="mt-4 text-4xl font-bold text-pink-600">Categories</h2>
 
-          <p className="mx-auto mt-6 max-w-3xl text-lg leading-8 text-slate-400">
+          <p className="mx-auto mt-6 max-w-3xl text-lg leading-8 text-slate-700 font-bold">
             Browse articles by category and discover tutorials, best practices,
             and practical software engineering guides for every stage of your
             learning journey.
